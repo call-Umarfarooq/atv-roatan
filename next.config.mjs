7deeab75becd39650f3244eb3f80f0e1ed828258
@1234@1234ab <call-Umarfarooq>
2026-02-09 06:25:36 +0500

    confifg

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'hk0gswgg8cgs0wg04scwsgok.72.62.242.32.sslip.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
